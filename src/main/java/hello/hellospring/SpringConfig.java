8cf09ace4c443649b2b21fa8f4dbc32b147e565d
choihyeji <hyejichoi14@gmail.com>
2023-10-20 20:24:04 +0900

    springdatajpa version

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,10 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
@@ -18,25 +15,32 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }
  */
+
+/*
     private final EntityManager em;
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
-
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
+*/
+    private final MemberRepository memberRepository;
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository() {
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
 
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository(dataSource);
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        //return new JpaMemberRepository(em);
 
-    }
+//    }
 }
